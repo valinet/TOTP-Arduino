9afe30ba489e20ab342473d6cdfbbc11dae68868
Valentin-Gabriel Radu <vali@valinet.ro>
2020-11-14 21:03:51 +0200

    Added support for SHA-256, 6/7/8 digits, fixed file paths to work with original Cryptosuite library

--- src/TOTP.cpp
+++ src/TOTP.cpp
@@ -5,7 +5,14 @@
 // Luca Dentella (http://www.lucadentella.it)
 
 #include "TOTP.h"
-#include "sha1.h"
+#define SHA 256
+#define DIGIT 8
+#define TIMESTEP 30
+#if SHA==1
+#include "../../Cryptosuite/Sha/sha1.h"
+#elif SHA==256
+#include "../../Cryptosuite/Sha/sha256.h"
+#endif
 
 // Init the library with the private key, its length and the timeStep duration
 TOTP::TOTP(uint8_t* hmacKey, int keyLength, int timeStep) {
@@ -20,7 +27,7 @@ TOTP::TOTP(uint8_t* hmacKey, int keyLength) {
 
 	_hmacKey = hmacKey;
 	_keyLength = keyLength;
-	_timeStep = 30;
+	_timeStep = TIMESTEP;
 };
 
 // Generate a code, using the timestamp provided
@@ -44,12 +51,20 @@ char* TOTP::getCodeFromSteps(long steps) {
 	_byteArray[7] = (int)((steps & 0XFF));
 	
 	// STEP 1, get the HMAC-SHA1 hash from counter and key
+	#if SHA==1
 	Sha1.initHmac(_hmacKey, _keyLength);
 	Sha1.write(_byteArray, 8);
 	_hash = Sha1.resultHmac();
+	// 19 for SHA-1, 31 for SHA-256, 63 for SHA-512 
+	_offset = _hash[20 - 1] & 0xF;
+	#elif SHA==256
+	Sha256.initHmac(_hmacKey, _keyLength);
+	Sha256.write(_byteArray, 8);
+	_hash = Sha256.resultHmac();
+	_offset = _hash[32 - 1] & 0xF;
+	#endif
 	
 	// STEP 2, apply dynamic truncation to obtain a 4-bytes string
-	_offset = _hash[20 - 1] & 0xF; 
 	_truncatedHash = 0;
 	for (int j = 0; j < 4; ++j) {
 		_truncatedHash <<= 8;
@@ -58,9 +73,21 @@ char* TOTP::getCodeFromSteps(long steps) {
 
 	// STEP 3, compute the OTP value
 	_truncatedHash &= 0x7FFFFFFF;
+	#if DIGIT==6
 	_truncatedHash %= 1000000;
+	#elif DIGIT==7
+	_truncatedHash %= 10000000;
+	#elif DIGIT==8
+	_truncatedHash %= 100000000;
+	#endif
 	
 	// convert the value in string, with heading zeroes
+	#if DIGIT==6
 	sprintf(_code, "%06ld", _truncatedHash);
+	#elif DIGIT==7
+	sprintf(_code, "%07ld", _truncatedHash);
+	#elif DIGIT==8
+	sprintf(_code, "%08ld", _truncatedHash);
+	#endif
 	return _code;
 }
